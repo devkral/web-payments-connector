4740a54e44525718d29c8ff915f66212f88603e9
alex <devkral@web.de>
2018-03-25 19:28:49 +0200

    fix tests

--- web_payments/django/test_django.py
+++ web_payments/django/test_django.py
@@ -4,16 +4,12 @@ from unittest.mock import patch, NonCallableMock
 from . import get_base_url
 
 class TestHelpers(TestCase):
-    @patch('web_payments.django.PAYMENT_USES_SSL', new_callable=NonCallableMock)
-    @patch('web_payments.django.PAYMENT_HOST', new_callable=NonCallableMock)
-    def test_text_get_base_url(self, host, use_ssl):
+    @patch('django.conf.settings.PAYMENT_HOST', new_callable=NonCallableMock)
+    def test_text_get_base_url(self, host):
         host.__str__ = lambda x: "example.com/string"
-        use_ssl = False
         self.assertEqual(get_base_url(), "https://example.com/string")
 
-    @patch('web_payments.django.PAYMENT_USES_SSL', new_callable=NonCallableMock)
-    @patch('web_payments.django.PAYMENT_HOST')
-    def test_callable_get_base_url(self, host, use_ssl):
+    @patch('django.conf.settings.PAYMENT_HOST')
+    def test_callable_get_base_url(self, host):
         host.return_value = "example.com/callable"
-        use_ssl = False
         self.assertEqual(get_base_url(), "https://example.com/callable")
