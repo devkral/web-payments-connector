feea92d6ba27ee83034fbd1aa6e8abe72deccb82
alex <devkral@web.de>
2018-03-30 00:34:04 +0200

    fix django import by hack, breaks translation interface

--- web_payments/translation.py
+++ web_payments/translation.py
@@ -2,6 +2,7 @@ import os
 import gettext
 import functools
 import threading
+import importlib
 
 
 __all__ = ["web_payments_translation_path", "set_language", "get_language", "Translation", "translation"]
@@ -32,12 +33,22 @@ def get_language():
 
 class _lazy_constant(object):
     def __init__(self, func, *args, **kwargs):
+        if not callable(func):
+            instance_path, func_name = func
+            module_path, instance_path = instance_path.rsplit(".", 1)
+            module = __import__(
+                str(module_path), globals(), locals(), [str(instance_path)])
+            instance_ = getattr(module, instance_path)
+            func = getattr(instance_, func_name)
+
         self.func = functools.partial(func, *args, **kwargs)
         # skip partial, provide func
         functools.update_wrapper(self, func)
 
     def deconstruct(self):
-        return (".".join([self.__class__.__module__, self.__class__.__qualname__]), [self.func.func, *self.func.args], self.func.keywords)
+        selfname = ".".join([self.__class__.__module__, self.__class__.__qualname__])
+        funcname = (self.func.func.__self__.instance_path, self.func.func.__qualname__.split(".", 1)[1])
+        return (selfname, [funcname, *self.func.args], self.func.keywords)
 
     def __eq__(self, obj):
         return self.func().__eq__(obj)
@@ -63,9 +74,11 @@ class _lazy_constant(object):
 class Translation(object):
     fallback = None
     domain = None
+    instance_path = None
     translation_path = None
-    def __init__(self, translation_path, domain="web_payments", fallback=None):
+    def __init__(self, instance_path, translation_path, domain="web_payments", fallback=None):
         self.translation_path = translation_path
+        self.instance_path = instance_path
         self.domain = domain
         if fallback:
             self.fallback = fallback
@@ -96,4 +109,4 @@ class Translation(object):
     def ngettext_lazy(self, msg, msgplural, n, cur_lang=None):
         return _lazy_constant(self.ngettext, msg, msgplural, n, cur_lang)
 
-translation = Translation(web_payments_translation_path, domain="web_payments")
+translation = Translation("web_payments.translation.translation", web_payments_translation_path, domain="web_payments")
--- web_payments_externalpayments/translation.py
+++ web_payments_externalpayments/translation.py
@@ -4,4 +4,4 @@ from web_payments.translation import Translation
 
 translation_path = os.path.join(os.path.dirname(__file__), "locale")
 
-translation = Translation(translation_path, domain="web_payments")
+translation = Translation("web_payments_externalpayments.translation", translation_path, domain="web_payments")
