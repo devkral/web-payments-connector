1e4ddfcbd61206a3314a078b2e9de63625bd3fa2
alex <devkral@web.de>
2018-04-06 15:58:48 +0200

    fix incomplete url

--- web_payments_dummy/django_dummy/models.py
+++ web_payments_dummy/django_dummy/models.py
@@ -1,10 +1,12 @@
+from urllib.parse import urljoin
+from web_payments.django import get_base_url
 from web_payments.django.models import BasePaymentWithAddress
 from django.urls import reverse
 
 class QPayment(BasePaymentWithAddress):
 
     def get_success_url(self):
-        return reverse("select-form")
+        return urljoin(get_base_url(), reverse("select-form"))
 
     def get_failure_url(self):
-        return reverse("select-form")
+        return urljoin(get_base_url(), reverse("select-form"))
