f79df9d7d979bdd232c4fc2b251e6d75f5cda19d
alex <devkral@web.de>
2023-05-26 11:34:14 +0200

    remove pytest

--- web_payments_dummy/django_dummy/apps.py
+++ web_payments_dummy/django_dummy/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DummyPaymentsConfig(AppConfig):
     name = "web_payments_dummy.django_dummy"
-    DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.BigAutoField"
