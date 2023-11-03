27c27866ababd013fecaaf31d72eec5c9bbac6d1
alex <devkral@web.de>
2023-11-03 23:16:48 +0100

    update deps

--- web_payments/django/apps.py
+++ web_payments/django/apps.py
@@ -1,10 +1,14 @@
 from django.apps import AppConfig
 from django.conf import settings
 
+
 class WebPaymentsConfig(AppConfig):
-    name = 'web_payments'
+    name = "web_payments"
+    default_auto_field = "django.db.models.BigAutoField"
+
     def ready(self):
-        from . import initialize, get_payment_model
+        from . import get_payment_model, initialize
+
         initialize()
         if getattr(settings, "DEBUG", False):
             get_payment_model().load_providers()
